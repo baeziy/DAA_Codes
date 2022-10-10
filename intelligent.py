2647706e803e6cd4ae3b8671c09cf4ed169ae693
Mustafa Kamal <mustfakmalik@gmail.com>
2022-10-10 23:59:31 +0500

    add intelligent code correct

--- intelligent.py
+++ intelligent.py
@@ -3,8 +3,10 @@ def foo(P):
     P = sorted(P)
     Stack = []
     for i in P:
-        while(not Stack and Stack[0][1] <= i[1]):
+        print(not Stack)
+        while(Stack and Stack[0][1] <= i[1]):
             Stack.pop()
         Stack.append(i)
 
+    return Stack
 print(foo([(2,1), (4,1), (6,1), (2,2), (3,2), (2,3), (1,4), (5,4), (3,5), (2,7)]))
