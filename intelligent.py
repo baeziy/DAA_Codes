790b7e97176c53d6e001246ac6e57b773d40afe0
Mustafa Kamal <mustfakmalik@gmail.com>
2022-10-10 23:59:31 +0500

    add intelligent code CRRCT

--- intelligent.py
+++ intelligent.py
@@ -3,8 +3,9 @@ def foo(P):
     P = sorted(P)
     Stack = []
     for i in P:
-        while(not Stack and Stack[0][1] <= i[1]):
+        while(Stack and Stack[-1][1] <= i[1]):
             Stack.pop()
         Stack.append(i)
 
-print(foo([(2,1), (4,1), (6,1), (2,2), (3,2), (2,3), (1,4), (5,4), (3,5), (2,7)]))
+    return Stack
+print(foo([(2,5), (3,13), (4,11), (5,1), (7,7), (9,10), (10,5), (12,12), (13,3), (14,10), (15,7)]))
